c23854ecac812cec89e15d4cf6d9ec9d29f4dd23
Durval Printes <durvalprintes@gmail.com>
2019-11-26 15:57:42 -0300

    adição de modelo, controlador e rotas da persona empresa

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
+const EmpresaControlador = require('./controllers/EmpresaControlador');
 
-routes.get('/', ProductController.raiz);
-routes.get('/products', ProductController.index);
-routes.get('/products/:id', ProductController.show);
-routes.post('/products', ProductController.store);
-routes.put('/products/:id', ProductController.update);
-routes.delete('/products/:id', ProductController.destroy);
+routes.get('/empresas', EmpresaControlador.index);
+routes.get('/empresas/:id', EmpresaControlador.show);
+routes.post('/empresas', EmpresaControlador.store);
+routes.delete('/empresas/:id', EmpresaControlador.destroy);
 
 module.exports = routes;
